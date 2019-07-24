76bb5b31a9d9aa4024e56eaa215180637c8bec94
kurrasowmya <sowmyakurra98@gmail.com>
2019-07-24 11:18:14 +0530

    @Lombok added

--- src/main/java/com/stackroute/MuzixApplicationTask/domain/Track.java
+++ src/main/java/com/stackroute/MuzixApplicationTask/domain/Track.java
@@ -1,9 +1,15 @@
 package com.stackroute.MuzixApplicationTask.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Track {
     //Make id as primary key
     @Id
@@ -11,48 +17,4 @@ public class Track {
     private int trackId;
     private String trackName;
     private String trackComment;
-    //Empty constructor
-    public Track()
-    {
-
-    }
-    //parameterized constructor
-    public Track(int trackId, String trackName, String trackComment) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.trackComment = trackComment;
-    }
-    //Getter and setter methods
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getTrackComment() {
-        return trackComment;
-    }
-
-    public void setTrackComment(String trackComment) {
-        this.trackComment = trackComment;
-    }
-    //Override the toString() method
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trackId=" + trackId +
-                ", trackName='" + trackName + '\'' +
-                ", trackComment='" + trackComment + '\'' +
-                '}';
-    }
 }
