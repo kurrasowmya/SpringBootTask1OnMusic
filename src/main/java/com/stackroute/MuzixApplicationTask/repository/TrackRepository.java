9c194c5b2829fc90077ebb99f39f17b44ca760a2
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-08-02 14:53:33 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/MuzixApplicationTask/repository/TrackRepository.java
+++ src/main/java/com/stackroute/MuzixApplicationTask/repository/TrackRepository.java
@@ -3,7 +3,7 @@ package com.stackroute.MuzixApplicationTask.repository;
 import com.stackroute.MuzixApplicationTask.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//Trackrepository that extends jparepository
+//Trackrepository that extends mongorepository
 @Repository
 public interface TrackRepository extends MongoRepository<Track,Integer> {
 }
