1328e4a23655ef34b5d6db665d2f18f4b2249ea8
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-08-04 18:38:04 +0530

    Update TrackServices.java

--- src/main/java/com/stackroute/MuzixApplicationTask/services/TrackServices.java
+++ src/main/java/com/stackroute/MuzixApplicationTask/services/TrackServices.java
@@ -3,22 +3,14 @@ package com.stackroute.MuzixApplicationTask.services;
 import com.stackroute.MuzixApplicationTask.domain.Track;
 import com.stackroute.MuzixApplicationTask.exception.TrackAlreadyExistsException;
 import com.stackroute.MuzixApplicationTask.exception.TrackNotFoundException;
-
 import java.util.List;
 
-public interface TrackServices {
+public interface TrackServices
+{
     //All the method declaration without haviing function body
-
-    public void saveTrack(Track track) throws TrackAlreadyExistsException;
-
+    public Track saveTrack(Track track) throws TrackAlreadyExistsException;
     public List<Track> getAllTracks();
-    public void deleteTrack(int trackId) throws TrackNotFoundException;
-
-
-
-    public boolean updateTrack(Track track,int trackId);
+    public Track deleteTrack(int trackId) throws TrackNotFoundException;
+    public Track updateTrack(Track track,int trackId) throws TrackNotFoundException;
     public List<Track> findByTrackName(String trackName) throws TrackNotFoundException;
-
-
-
 }
