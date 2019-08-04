0f12e722eda8222ad051a9e4aa7834061532d2b9
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-08-04 19:37:33 +0530

    Update TrackServicesImpl.java

--- src/main/java/com/stackroute/MuzixApplicationTask/services/TrackServicesImpl.java
+++ src/main/java/com/stackroute/MuzixApplicationTask/services/TrackServicesImpl.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 public class TrackServicesImpl implements TrackServices {
 
-    TrackRepository trackrepository;
+    private TrackRepository trackrepository;
     @Autowired
     public TrackServicesImpl(TrackRepository trackRepository)
     {
