1511a193f79ff986b4ed820b15faebaaf46660ba
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-08-04 19:28:56 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/MuzixApplicationTask/repository/TrackRepository.java
+++ src/main/java/com/stackroute/MuzixApplicationTask/repository/TrackRepository.java
@@ -4,11 +4,9 @@ import com.stackroute.MuzixApplicationTask.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 //Trackrepository that extends jparepository
-
 public interface TrackRepository extends JpaRepository<Track,Integer> {
     @Query("SELECT p FROM Track p WHERE p.trackName like ?1")
     List<Track> findByTrackName(String trackname);
